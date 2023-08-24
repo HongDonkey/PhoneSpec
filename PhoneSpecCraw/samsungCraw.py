0b67ef76bc9821e5b85e664ba58be19a0a0cac48
psw09 <tjddns09@gmail.com>
2023-08-24 22:43:25 +0900

    test2

--- PhoneSpecCraw/samsungCraw.py
+++ PhoneSpecCraw/samsungCraw.py
@@ -8,6 +8,7 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 print("test123123123")
+print("test22")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
