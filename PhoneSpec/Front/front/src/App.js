73fd530626471270059115c56455f2348c0cc204
HongDonkey <hdk1992@naver.com>
2023-11-21 21:47:33 +0900

    20231121 갤럭시 기기 DB추가 html, css 프리템플릿 추가

--- PhoneSpec/Front/front/src/App.js
+++ PhoneSpec/Front/front/src/App.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import FirstPage from "./home/firstPage";
+import Top from "./home/top";
 
 function App() {
    const [hello, setHello] = useState('')
@@ -14,17 +14,19 @@ function App() {
 
     return (
         <div>
-            백엔드에서 가져온 데이터입니다 : {hello}
+            <span>
         <BrowserRouter>
             <Routes>
                 {/* 웹 서비스 소개 페이지 */}
-                <Route path="/" element={<FirstPage />} />
+                <Route path="/" element={<Top />} />
                 {/* <SignIn /> */}
                 {/* <Route path="/signin" element={<SignIn />} /> */}
                 {/* <LogIn /> */}
                 {/* <Route path="/login" element={<LogIn />} /> */}
             </Routes>
         </BrowserRouter> 
+            </span>
+            백엔드에서 가져온 데이터입니다 : {hello}
             
         </div>
         
