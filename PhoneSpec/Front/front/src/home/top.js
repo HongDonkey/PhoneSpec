c1efa068211c9859d659af23f649051bdee40d06
HongDonkey <hdk1992@naver.com>
2023-11-23 21:33:37 +0900

    20231123 이미지 파일과 css파일을 각각 public과 src경로로 나눠줌

--- PhoneSpec/Front/front/src/home/top.js
+++ PhoneSpec/Front/front/src/home/top.js
@@ -1,5 +1,5 @@
-import "./css/bootstrap.min.css"
-import "./css/style.css"
+import "../css/bootstrap.min.css"
+import "../css/style.css"
 
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
                     <nav class="navbar navbar-expand-lg bg-white navbar-light py-0 px-4">
                         <a href="index.html" class="navbar-brand d-flex align-items-center text-center">
                             <div class="icon p-2 me-2">
-                                <img class="img-fluid" src="/icon-deal.png" alt="Icon" className="width: 30px; height: 30px;"/>
+                                <img class="img-fluid" src={process.env.PUBLIC_URL + "img/icon-deal.png"} alt="Icon" className="width: 30px; height: 30px;"/>
                             </div>
                             <h1 class="m-0 text-primary">Makaan</h1>
                         </a>
@@ -20,7 +20,7 @@ function App() {
                                 <a href="index.html" class="nav-item nav-link">Home</a>
                                 <a href="about.html" class="nav-item nav-link">About</a>
                                 <div class="nav-item dropdown">
-                                    <a href="#" className="nav-link dropdown-toggle active" data-bs-toggle="dropdown">Property</a>
+                                    <a href="/" className="nav-link dropdown-toggle active" data-bs-toggle="dropdown">Property</a>
                                     <div class="dropdown-menu rounded-0 m-0">
                                         <a href="property-list.html" class="dropdown-item active">Property List</a>
                                         <a href="property-type.html" class="dropdown-item">Property Type</a>
@@ -28,7 +28,7 @@ function App() {
                                     </div>
                                 </div>
                                 <div class="nav-item dropdown">
-                                    <a href="#" className="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+                                    <a href="/" className="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
                                     <div class="dropdown-menu rounded-0 m-0">
                                         <a href="testimonial.html" class="dropdown-item">Testimonial</a>
                                         <a href="404.html" class="dropdown-item">404 Error</a>
@@ -36,12 +36,12 @@ function App() {
                                 </div>
                                 <a href="contact.html" class="nav-item nav-link">Contact</a>
                             </div>
-                            <a href="" className="btn btn-primary px-3 d-none d-lg-flex">Add Property</a>
+                            <a href="/" className="btn btn-primary px-3 d-none d-lg-flex">Add Property</a>
                         </div>
                     </nav>
         </div>
     )
-    {/* <!-- Navbar End --> */}
+    // <!-- Navbar End --> 
     
 }
 
