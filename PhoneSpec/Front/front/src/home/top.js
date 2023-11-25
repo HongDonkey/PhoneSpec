e45faf6172b412b90b669bda6b21ff4367839a83
HongDonkey <hdk1992@naver.com>
2023-11-26 00:47:31 +0900

    20231126 bootstrap 공부 후 적용

--- PhoneSpec/Front/front/src/home/top.js
+++ PhoneSpec/Front/front/src/home/top.js
@@ -1,16 +1,48 @@
 import "../css/bootstrap.min.css"
 import "../css/style.css"
 
+function Head(){
+    return
+
+
+}
 
 function App() {
     return(
-        <div class="container-fluid nav-bar bg-transparent">
+        
+        <div>
+        {/* <!-- Google Web Fonts --> */}
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600&family=Inter:wght@700;800&display=swap" rel="stylesheet"/>
+        
+        {/* <!-- Icon Font Stylesheet --> */}
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet"/>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet"/>
+
+        {/* <!-- Libraries Stylesheet --> */}
+        <link href="lib/animate/animate.min.css" rel="stylesheet"/>
+        <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet"/>
+
+        {/* <!-- Customized Bootstrap Stylesheet --> */}
+        <link href="css/bootstrap.min.css" rel="stylesheet"/>
+
+        {/* <!-- Template Stylesheet --> */}
+        <link href="css/style.css" rel="stylesheet"/>
+        {/* <!-- JavaScript Libraries --> */}
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="lib/wow/wow.min.js"></script>
+        <script src="lib/easing/easing.min.js"></script>
+        <script src="lib/waypoints/waypoints.min.js"></script>
+        <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+        <link rel="sytlesheet" href="/css/bootstrap.min.css"/> 
                     <nav class="navbar navbar-expand-lg bg-white navbar-light py-0 px-4">
                         <a href="index.html" class="navbar-brand d-flex align-items-center text-center">
                             <div class="icon p-2 me-2">
                                 <img class="img-fluid" src={process.env.PUBLIC_URL + "img/icon-deal.png"} alt="Icon" className="width: 30px; height: 30px;"/>
                             </div>
-                            <h1 class="m-0 text-primary">Makaan</h1>
+                            <h1 class="m-0 text-primary">PhoneSpec</h1>
                         </a>
                         <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
                             <span class="navbar-toggler-icon"></span>
@@ -39,7 +71,10 @@ function App() {
                             <a href="/" className="btn btn-primary px-3 d-none d-lg-flex">Add Property</a>
                         </div>
                     </nav>
+        
         </div>
+
+    
     )
     // <!-- Navbar End --> 
     
